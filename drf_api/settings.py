f528cce028a153118beeb11795e87861ba22b371
Bhagyashri Yogesh Patil <bhagyashriyogeshpatil@gmail.com>
2024-09-05 14:05:21 +0000

    added profiles detail view

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-bhagyashriyogesh-drfapi-tzyfgtqpk4e.ws.codeinstitute-ide.net',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
